efa5bcc4e9cdc3a36339c75a3e9e688cedefaf27
Isa <cw3997@nyu.edu>
2025-09-04 23:59:13 -0400

    Update start.py

--- start.py
+++ start.py
@@ -7,5 +7,5 @@ Test Program
 # Once done, commit the changes
 
 
-print ("Hi there, my name is Mark Ramos")
-print ("My GitHub username is -------")
+print ("Hi there, my name is Chen Wang")
+print ("My GitHub username is Isa-be-lla")
